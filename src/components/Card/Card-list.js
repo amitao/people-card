7d69a90b329f9f1748885fc45df5665dd3282a59
Amie Thao <amie.c.thao@gmail.com>
2019-12-08 20:50:58 -0600

    add img url to display images

--- src/components/Card/Card-list.js
+++ src/components/Card/Card-list.js
@@ -4,6 +4,7 @@ import './Card.css';
 export const CardList = (props) => {
   return (
     <div className="card-container">
+    <img src="https://picsum.photos/id/1/200/300" alt="image"/>
       <h1>{props.people.name}</h1>
     </div>
   )

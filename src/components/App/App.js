9f2765d5730a984fb2cfc5dc9005c34da3849b7a
Amie Thao <amie.c.thao@gmail.com>
2019-12-08 19:29:12 -0600

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    peoples: []
+  }
+
+
+  componentDidMount() {
+    // fetch the API url and returns a Promise
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())  // returns a json object
+    .then( users => this.setState({peoples: users})) // set people in the array
+  }
+
+  render() {
+    return(
+      <div className="App">
+      <Header />
+        {this.state.peoples.map( people => {
+          return (
+            <h1 key={people.id}>{people.name}</h1>
+          )
+         })
+        }
+      </div>
+    )
+  }
+}
+
+export default App;

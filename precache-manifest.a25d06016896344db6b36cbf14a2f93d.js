8e8e7652d2f0da1a6dff99a7524a0e5b0422d7b5
Amie Thao <amie.c.thao@gmail.com>
2019-12-10 21:06:49 -0600

    Updates

---
+++ precache-manifest.a25d06016896344db6b36cbf14a2f93d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c040035817d8dea82de2677326c923c5",
+    "url": "/people-card/index.html"
+  },
+  {
+    "revision": "8bc246df3e8c9cece17c",
+    "url": "/people-card/static/css/main.5664bddf.chunk.css"
+  },
+  {
+    "revision": "e89545c11b4e66fec596",
+    "url": "/people-card/static/js/2.f1a41d96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/people-card/static/js/2.f1a41d96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8bc246df3e8c9cece17c",
+    "url": "/people-card/static/js/main.c0246225.chunk.js"
+  },
+  {
+    "revision": "16e3fc50f4ed3d7ffa29",
+    "url": "/people-card/static/js/runtime-main.a4b43eda.js"
+  }
+]);
